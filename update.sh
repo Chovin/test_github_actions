6ef27eb6ce05eec8543190eed7ff4bda6ef47454
chovin <chovin@users.noreply.github.com>
2023-10-12 12:56:34 +1000

    more logging

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ echo ""
 echo "----------------------------"
 echo $(date)
 echo "----------------------------"
-
+echo ">>> logging in to $REGISTRY"
 # login to github container registry
 # docker login ghcr.io -u $ -p ${TOKEN}
 echo ${TOKEN} | docker login $REGISTRY -u $ --password-stdin
@@ -20,6 +20,7 @@ echo ${TOKEN} | docker login $REGISTRY -u $ --password-stdin
 # TODO: change hardcoded path later
 cd /home/ubuntu/test_github_actions
 
+echo ">>> git pull"
 # update source just cause
 git pull
 
@@ -29,15 +30,18 @@ git pull
 # remove local image
 # docker rmi ghcr.io/tropicode-guam/blue-guam-frontend:latest
 
+echo ">>> updating image"
 # download docker image
 docker pull ${REGISTRY}/chovin/test_github_actions:latest
 
+echo ">>> recreating service"
 docker compose -f docker/production/docker-compose.yml up -d
 
+echo ">>> logging out of $REGISTRY"
 # remove token from docker config
 docker logout $REGISTRY
 
+echo ">>> docker pulled"
 docker images ghcr.io/chovin/test_github_actions:latest
-echo "docker pulled"
-echo "should be at git commit:"
+echo ">>> should be at git commit:"
 git log -n1
